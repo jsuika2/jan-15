f5c73c727e10e33dc7198a621ac14c2284624516
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-16 20:23:22 -0500

    Quizs formulas added

--- Associating_js/JS/script.js
+++ Associating_js/JS/script.js
@@ -1,8 +1,12 @@
-//area of the triangle is half of the width times the height
-
-var width =4;
-var height=5;
-var area=width * height/2;
-console.log(area);
 
 
+
+
+var quiz1=87;
+var quiz2=100;
+var quiz3=60;
+var quiz4=80;
+
+var average= (quiz1+quiz2+quiz3+quiz4)/4;
+
+console.log(average);
